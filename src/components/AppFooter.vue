24a621908bfa67409fffd8e8038d1d1907fab582
takeshiyabiku <shiroukitsunee@gmail.com>
2025-05-22 09:44:29 -0300

    On branch main Your branch is up to date with 'origin/main'. Changes to be committed: 	modified:   src/components/AppFooter.vue

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -3,7 +3,7 @@
     <div class="footer-links">
       <a href="/faq">FAQ</a>
       <a href="https://ygoprodeck.com/api-guide/">Feito com ygoprodeckapi</a>
-      <a href="/contact-us">Contate-nos</a>
+      <a href="https://github.com/takeshiyabiku/big-world-app">GitHub</a>
       <a href="/terms">Termos de serviço</a>
       <a href="/privacy">Política de privacidade</a>
     </div>
